a529eddb38f318972eb715772ea87a921bad49b9
venkatesh_murali <venkateshmurali0@gmail.com>
2022-04-15 19:14:34 +0530

    Stepper Style issue

--- src/components/experience/index.css.js
+++ src/components/experience/index.css.js
@@ -1,5 +1,4 @@
 import { makeStyles } from "@mui/styles";
-import theme from "../../style/theme";
 
 export const styles = makeStyles({
   rootContainer: {
@@ -15,33 +14,25 @@ export const styles = makeStyles({
     "@media only screen and (min-width: 992px)": {},
   },
   stepper: {
-    "& .css-1u4zpwo-MuiSvgIcon-root-MuiStepIcon-root.Mui-active": {
-      color: `#64ffda !important`,
-    },
-    "& .css-1u4zpwo-MuiSvgIcon-root-MuiStepIcon-root.Mui-completed": {
-      color: `#64ffda !important`,
-    },
-    "& .css-qivjh0-MuiStepLabel-label.Mui-completed": {
-      color: `#64ffda !important`,
-    },
-    "& .css-qivjh0-MuiStepLabel-label.Mui-active": {
-      color: `#64ffda !important`,
-    },
-    "& .css-16ubnlw-MuiStepLabel-labelContainer": {
-      color: theme.text.bodyText2,
-    },
-  },
-  newStepper: {
     "& .MuiStepLabel-label": {
       color: "#64ffda",
     },
-    "&  .MuiStepLabel-label .Mui-active": {
+    "&  .MuiStepLabel-label.Mui-active": {
+      color: "#64ffda !important",
+    },
+    "& .MuiStepLabel-label.Mui-completed": {
       color: "#64ffda",
     },
-    "& .MuiStepLabel-label .Mui-completed": {
+    "& .MuiStepLabel-label.Mui-disabled": {
       color: "#64ffda",
     },
-    "& .MuiStepLabel-label .Mui-disabled": {
+    "& .MuiStepIcon-root.Mui-active": {
+      color: "#64ffda",
+    },
+    "& .MuiStepIcon-root.Mui-completed": {
+      color: "#64ffda",
+    },
+    "& .MuiStepIcon-root.Mui-disabled": {
       color: "#64ffda",
     },
   },
